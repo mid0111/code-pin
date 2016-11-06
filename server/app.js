f761f2a4f940614bf88c4409993bfba710abf746
mid0111 <m.tajima0111@gmail.com>
2016-11-07 08:15:29 +0900

    Add gzip compression for static file.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 // var favicon = require('serve-favicon');
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
