06f3e9720913af50a151bd0b6147f1d2bfceed05
mid0111 <m.tajima0111@gmail.com>
2016-11-06 22:08:40 +0900

    Add uglify option for production.

--- webpack/build.webpack.js
+++ webpack/build.webpack.js
@@ -1,12 +1,23 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = require('./base.config');
 
 config.plugins.unshift(
+  new webpack.DefinePlugin({
+    'process.env':{
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
   // Clean build folder(s) before building.
   new CleanWebpackPlugin(['server/public'], {
     root: path.join(__dirname, '..'),
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
   })
 );
 
